01fca87220803f0c2c782b24ab9fa1cc9903b6b0
Noman14611 <noumantareen316@gmail.com>
2025-06-15 14:21:06 +0500

    main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-from feeapp.py import run_fee_app
-
-# You can add more apps later like attendance or salary
-app = st.sidebar.selectbox("Select App", ["Fee Submission"])
-
-if app == "Fee Submission":
-    run_fee_app()
