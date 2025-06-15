f35eb94b3b9549227ac8ae8a0e6e1bbc46c1ce7c
Noman14611 <noumantareen316@gmail.com>
2025-06-15 11:16:56 +0500

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from feeapp import run_fee_app
+from feeapp.py import run_fee_app
 
 # You can add more apps later like attendance or salary
 app = st.sidebar.selectbox("Select App", ["Fee Submission"])
